723d8783fdb385912833732ac5ccbbd7489f3e73
agusclique <agustina2003zalazar@gmail.com>
2026-07-15 23:19:12 -0300

    feat: update artist profile portrait URLs
    Replace local image paths with external sources to ensure correct rendering of artist portraits.

--- src/data/mockData.ts
+++ src/data/mockData.ts
@@ -10,7 +10,7 @@ export const ARTISTS: Artist[] = [
     id: 'art-1',
     name: 'Valeria Vega',
     role: 'Fundadora & Artista Principal',
-    portrait: '/assets/images/valeria_artist_1782919840521.jpg', // Our generated asset!
+    portrait: 'https://i.pinimg.com/736x/b2/37/6a/b2376a03f7a449cf808757650cbddf85.jpg', // Our generated asset!
     bio: 'Con más de una década de experiencia internacional, Valeria ha desarrollado un estilo inconfundible que fusiona el misticismo gótico, el grabado medieval y el blackwork de alto contraste. Su enfoque se centra en crear piezas fluidas que interactúan dinámicamente con la musculatura del cuerpo.',
     specialties: ['Blackwork', 'Dark Art', 'Grabado Medieval', 'Horror'],
     instagram: '@valeria.nocturna',
@@ -43,7 +43,7 @@ export const ARTISTS: Artist[] = [
     id: 'art-4',
     name: 'Damián Frost',
     role: 'Artista Residente Tradicional',
-    portrait: 'https://images.unsplash.com/photo-1620122303020-43ec4b6cf7f8?q=80&w=600&auto=format&fit=crop',
+    portrait: 'https://i.pinimg.com/1200x/90/c9/1f/90c91fa1828b6451336f0d53ead5aee7.jpg',
     bio: 'Damián rinde homenaje a los pioneros del tatuaje occidental con su estilo Tradicional Americano. Sus diseños destacan por líneas extraordinariamente gruesas, pigmentos negros impenetrables y una solidez que garantiza que la pieza permanezca intacta por décadas.',
     specialties: ['Traditional', 'Neo-Traditional', 'Lettering heavy', 'Color Sólido'],
     instagram: '@damian.frost.tattoo',
@@ -89,7 +89,7 @@ export const PORTFOLIO_ITEMS: PortfolioItem[] = [
   {
     id: 'port-1',
     title: 'La Consagración del Caos',
-    imageUrl: '/assets/images/portfolio_bat_1782922738242.jpg',
+    imageUrl: 'https://instagram.fepa8-1.fna.fbcdn.net/v/t51.82787-15/670272648_18094125440515789_6344895025006018279_n.webp?stp=dst-webp_p720x720&_nc_cat=107&ig_cache_key=Mzg3NDYzODU2MTc0MDI4NjMwOA%3D%3D.3-ccb7-5&ccb=7-5&_nc_sid=58cdad&efg=eyJ2ZW5jb2RlX3RhZyI6IkNBUk9VU0VMX0lURU0ueHBpZHMuMTA4MC5zZHIucmVndWxhcl9waG90by5DMyJ9&_nc_ohc=P4eXuMOK9UMQ7kNvwFw62xx&_nc_oc=AdqKmNIGaG0qSRFg4izWbYbX2Kq2TSgMOE4G5r1D4_DuOwcFbdQF5TmqiiX-88nBz9hbVJrlQVcBUerq4_3659uQ&_nc_ad=z-m&_nc_cid=0&_nc_zt=23&_nc_ht=instagram.fepa8-1.fna&_nc_gid=6dU_cuKV1r1c8LI2FGFfCQ&_nc_ss=7a22e&oh=00_AQC2EYQ3GI5_Eoaes0kRyOv3qM2XvR2RH93t3TEDkRUrbw&oe=6A5E1C0A',
     artistId: 'art-1',
     bodyPart: 'Espalda',
     style: 'Blackwork',
@@ -98,13 +98,13 @@ export const PORTFOLIO_ITEMS: PortfolioItem[] = [
     colorType: 'Black & Grey',
     description: 'Pieza de espalda completa inspirada en grabados góticos alemanes del siglo XV. Representa figuras místicas entrelazadas por patrones orgánicos.',
     healingPhotos: [
-      '/assets/images/portfolio_bat_1782922738242.jpg'
+      'https://instagram.fepa8-1.fna.fbcdn.net/v/t51.82787-15/670272648_18094125440515789_6344895025006018279_n.webp?stp=dst-webp_p720x720&_nc_cat=107&ig_cache_key=Mzg3NDYzODU2MTc0MDI4NjMwOA%3D%3D.3-ccb7-5&ccb=7-5&_nc_sid=58cdad&efg=eyJ2ZW5jb2RlX3RhZyI6IkNBUk9VU0VMX0lURU0ueHBpZHMuMTA4MC5zZHIucmVndWxhcl9waG90by5DMyJ9&_nc_ohc=P4eXuMOK9UMQ7kNvwFw62xx&_nc_oc=AdqKmNIGaG0qSRFg4izWbYbX2Kq2TSgMOE4G5r1D4_DuOwcFbdQF5TmqiiX-88nBz9hbVJrlQVcBUerq4_3659uQ&_nc_ad=z-m&_nc_cid=0&_nc_zt=23&_nc_ht=instagram.fepa8-1.fna&_nc_gid=6dU_cuKV1r1c8LI2FGFfCQ&_nc_ss=7a22e&oh=00_AQC2EYQ3GI5_Eoaes0kRyOv3qM2XvR2RH93t3TEDkRUrbw&oe=6A5E1C0A'
     ]
   },
   {
     id: 'port-2',
     title: 'Ecuación Sagrada',
-    imageUrl: '/assets/images/portfolio_ghost_1782922754442.jpg',
+    imageUrl: 'https://instagram.fepa14-1.fna.fbcdn.net/v/t51.82787-15/652058043_18090671684515789_8236650776401866259_n.webp?stp=dst-webp_p720x720&_nc_cat=100&ig_cache_key=Mzg1NDM2OTU1MjE2ODA2ODkwNQ%3D%3D.3-ccb7-5&ccb=7-5&_nc_sid=58cdad&efg=eyJ2ZW5jb2RlX3RhZyI6IkNBUk9VU0VMX0lURU0ueHBpZHMuMTA4MC5zZHIucmVndWxhcl9waG90by5DMyJ9&_nc_ohc=Z1xq_8VxfZAQ7kNvwGVLSA9&_nc_oc=AdoFpDdokDU6muihROhQMrsZDnWiop8Tkn0CFeAxZozgK4RsBe9WLjzGB120CWBabaOk-GP750ujoCN-C8IBgIvG&_nc_ad=z-m&_nc_cid=0&_nc_zt=23&_nc_ht=instagram.fepa14-1.fna&_nc_gid=6dU_cuKV1r1c8LI2FGFfCQ&_nc_ss=7a22e&oh=00_AQAicUaM213VJf3xWBCo8Atmi2I6JbW4GkLYTY9Vpl-hdg&oe=6A5DF68B',
     artistId: 'art-2',
     bodyPart: 'Brazo',
     style: 'Geometric',
@@ -113,13 +113,13 @@ export const PORTFOLIO_ITEMS: PortfolioItem[] = [
     colorType: 'Black & Grey',
     description: 'Mandala simétrico ultra-detallado con micropuntillismo degradado y líneas concéntricas de 0.15mm.',
     healingPhotos: [
-      '/assets/images/portfolio_ghost_1782922754442.jpg'
+      'https://instagram.fepa14-1.fna.fbcdn.net/v/t51.82787-15/652058043_18090671684515789_8236650776401866259_n.webp?stp=dst-webp_p720x720&_nc_cat=100&ig_cache_key=Mzg1NDM2OTU1MjE2ODA2ODkwNQ%3D%3D.3-ccb7-5&ccb=7-5&_nc_sid=58cdad&efg=eyJ2ZW5jb2RlX3RhZyI6IkNBUk9VU0VMX0lURU0ueHBpZHMuMTA4MC5zZHIucmVndWxhcl9waG90by5DMyJ9&_nc_ohc=Z1xq_8VxfZAQ7kNvwGVLSA9&_nc_oc=AdoFpDdokDU6muihROhQMrsZDnWiop8Tkn0CFeAxZozgK4RsBe9WLjzGB120CWBabaOk-GP750ujoCN-C8IBgIvG&_nc_ad=z-m&_nc_cid=0&_nc_zt=23&_nc_ht=instagram.fepa14-1.fna&_nc_gid=6dU_cuKV1r1c8LI2FGFfCQ&_nc_ss=7a22e&oh=00_AQAicUaM213VJf3xWBCo8Atmi2I6JbW4GkLYTY9Vpl-hdg&oe=6A5DF68B'
     ]
   },
   {
     id: 'port-3',
     title: 'Despertar de la Ciber-Geisha',
-    imageUrl: '/assets/images/portfolio_seraphim_1782922764899.jpg',
+    imageUrl: 'https://instagram.fepa8-1.fna.fbcdn.net/v/t51.82787-15/695677163_18097553168515789_1628480445891358663_n.webp?stp=dst-webp_p720x720&_nc_cat=109&ig_cache_key=Mzg5NDg4NjY2NzAxMzQ4NTAzNA%3D%3D.3-ccb7-5&ccb=7-5&_nc_sid=58cdad&efg=eyJ2ZW5jb2RlX3RhZyI6IkNBUk9VU0VMX0lURU0ueHBpZHMuMTA4MC5zZHIucmVndWxhcl9waG90by5DMyJ9&_nc_ohc=_hjOFqVV1I8Q7kNvwF9HZPB&_nc_oc=Ado-EWXpDvhQ-gL5iaytWNjA0gMKZylNYoXxcLBAQlt5yNhirew6iJBaT7uTHIWxSgUO56rZ7IK5sNe5vf8cQnAp&_nc_ad=z-m&_nc_cid=0&_nc_zt=23&_nc_ht=instagram.fepa8-1.fna&_nc_gid=6dU_cuKV1r1c8LI2FGFfCQ&_nc_ss=7a22e&oh=00_AQCFJbfFjanoVkRxfNCPsOxbojsV1xYRCnmG4Qxyfioavg&oe=6A5DFE4D',
     artistId: 'art-3',
     bodyPart: 'Brazo',
     style: 'Anime',
@@ -128,13 +128,13 @@ export const PORTFOLIO_ITEMS: PortfolioItem[] = [
     colorType: 'Black & Grey',
     description: 'Diseño neo-manga fusionado con elementos mecánicos cibernéticos y alto contraste en fondos sólidos.',
     healingPhotos: [
-      '/assets/images/portfolio_seraphim_1782922764899.jpg'
+      'https://instagram.fepa8-1.fna.fbcdn.net/v/t51.82787-15/695677163_18097553168515789_1628480445891358663_n.webp?stp=dst-webp_p720x720&_nc_cat=109&ig_cache_key=Mzg5NDg4NjY2NzAxMzQ4NTAzNA%3D%3D.3-ccb7-5&ccb=7-5&_nc_sid=58cdad&efg=eyJ2ZW5jb2RlX3RhZyI6IkNBUk9VU0VMX0lURU0ueHBpZHMuMTA4MC5zZHIucmVndWxhcl9waG90by5DMyJ9&_nc_ohc=_hjOFqVV1I8Q7kNvwF9HZPB&_nc_oc=Ado-EWXpDvhQ-gL5iaytWNjA0gMKZylNYoXxcLBAQlt5yNhirew6iJBaT7uTHIWxSgUO56rZ7IK5sNe5vf8cQnAp&_nc_ad=z-m&_nc_cid=0&_nc_zt=23&_nc_ht=instagram.fepa8-1.fna&_nc_gid=6dU_cuKV1r1c8LI2FGFfCQ&_nc_ss=7a22e&oh=00_AQCFJbfFjanoVkRxfNCPsOxbojsV1xYRCnmG4Qxyfioavg&oe=6A5DFE4D'
     ]
   },
   {
     id: 'port-4',
     title: 'El Duelo Inmortal',
-    imageUrl: '/assets/images/portfolio_palms_1782922776487.jpg',
+    imageUrl: 'https://instagram.fepa8-1.fna.fbcdn.net/v/t51.82787-15/720509365_18101280593515789_4023535422940916516_n.webp?stp=dst-webp_p720x720&_nc_cat=105&ig_cache_key=MzkxNTkyMTg1MjcxNDYzNzMyOQ%3D%3D.3-ccb7-5&ccb=7-5&_nc_sid=58cdad&efg=eyJ2ZW5jb2RlX3RhZyI6IkNBUk9VU0VMX0lURU0ueHBpZHMuMTA4MC5zZHIucmVndWxhcl9waG90by5DMyJ9&_nc_ohc=QZbADpsSTxMQ7kNvwEF86I0&_nc_oc=AdpgEJiCmiGlso0w1f0TbF42o1b9zHql4VnHPP5p_WSAEpjI0QSNnCy3lMUUfgkS9wUbeRNbtFIBrJL1pa39xZZ7&_nc_ad=z-m&_nc_cid=0&_nc_zt=23&_nc_ht=instagram.fepa8-1.fna&_nc_gid=6dU_cuKV1r1c8LI2FGFfCQ&_nc_ss=7a22e&oh=00_AQC2P_f2h8SfWRr1k9QEOUodEUEt3-CeDGHCNo-lV8BVjA&oe=6A5E1C76',
     artistId: 'art-4',
     bodyPart: 'Pecho',
     style: 'Traditional',
@@ -143,13 +143,13 @@ export const PORTFOLIO_ITEMS: PortfolioItem[] = [
     colorType: 'Color',
     description: 'Composición clásica americana de águila y serpiente con paleta reducida a negro saturado, rojo sangre y amarillo mostaza.',
     healingPhotos: [
-      '/assets/images/portfolio_palms_1782922776487.jpg'
+      'https://instagram.fepa8-1.fna.fbcdn.net/v/t51.82787-15/720509365_18101280593515789_4023535422940916516_n.webp?stp=dst-webp_p720x720&_nc_cat=105&ig_cache_key=MzkxNTkyMTg1MjcxNDYzNzMyOQ%3D%3D.3-ccb7-5&ccb=7-5&_nc_sid=58cdad&efg=eyJ2ZW5jb2RlX3RhZyI6IkNBUk9VU0VMX0lURU0ueHBpZHMuMTA4MC5zZHIucmVndWxhcl9waG90by5DMyJ9&_nc_ohc=QZbADpsSTxMQ7kNvwEF86I0&_nc_oc=AdpgEJiCmiGlso0w1f0TbF42o1b9zHql4VnHPP5p_WSAEpjI0QSNnCy3lMUUfgkS9wUbeRNbtFIBrJL1pa39xZZ7&_nc_ad=z-m&_nc_cid=0&_nc_zt=23&_nc_ht=instagram.fepa8-1.fna&_nc_gid=6dU_cuKV1r1c8LI2FGFfCQ&_nc_ss=7a22e&oh=00_AQC2P_f2h8SfWRr1k9QEOUodEUEt3-CeDGHCNo-lV8BVjA&oe=6A5E1C76'
     ]
   },
   {
     id: 'port-5',
     title: 'Ojo del Abismo Celestial',
-    imageUrl: '/assets/images/portfolio_thumbs_1782922785223.jpg',
+    imageUrl: 'https://instagram.fepa8-1.fna.fbcdn.net/v/t51.82787-15/656722383_18091844531515789_8318997019918823621_n.webp?stp=dst-webp_p720x720&_nc_cat=101&ig_cache_key=Mzg2MDE2NDk2MjgwNzY4MDc4Mg%3D%3D.3-ccb7-5&ccb=7-5&_nc_sid=58cdad&efg=eyJ2ZW5jb2RlX3RhZyI6IkNBUk9VU0VMX0lURU0ueHBpZHMuMTA4MC5zZHIucmVndWxhcl9waG90by5DMyJ9&_nc_ohc=8ZfOqrpJF_IQ7kNvwHMT8eY&_nc_oc=Adp0onIFFthz5PkBVNbXaNmrF2yjW2abeOMEa_HfR2jCHVJ_cR8AjUN8c6J5xbr-l6Of90vNJ0GFpdBgX8V5MzYc&_nc_ad=z-m&_nc_cid=0&_nc_zt=23&_nc_ht=instagram.fepa8-1.fna&_nc_gid=6dU_cuKV1r1c8LI2FGFfCQ&_nc_ss=7a22e&oh=00_AQBaW2FzunI3JebOb0I1iyljnuHDrPTExlkbyTsB6bNrFQ&oe=6A5E2491',
     artistId: 'art-1',
     bodyPart: 'Mano',
     style: 'Dark Art',
@@ -158,13 +158,13 @@ export const PORTFOLIO_ITEMS: PortfolioItem[] = [
     colorType: 'Black & Grey',
     description: 'Ojo esotérico hiperdetallado en el dorso de la mano con lágrimas geométricas que descienden hacia las falanges.',
     healingPhotos: [
-      '/assets/images/portfolio_thumbs_1782922785223.jpg'
+      'https://instagram.fepa8-1.fna.fbcdn.net/v/t51.82787-15/656722383_18091844531515789_8318997019918823621_n.webp?stp=dst-webp_p720x720&_nc_cat=101&ig_cache_key=Mzg2MDE2NDk2MjgwNzY4MDc4Mg%3D%3D.3-ccb7-5&ccb=7-5&_nc_sid=58cdad&efg=eyJ2ZW5jb2RlX3RhZyI6IkNBUk9VU0VMX0lURU0ueHBpZHMuMTA4MC5zZHIucmVndWxhcl9waG90by5DMyJ9&_nc_ohc=8ZfOqrpJF_IQ7kNvwHMT8eY&_nc_oc=Adp0onIFFthz5PkBVNbXaNmrF2yjW2abeOMEa_HfR2jCHVJ_cR8AjUN8c6J5xbr-l6Of90vNJ0GFpdBgX8V5MzYc&_nc_ad=z-m&_nc_cid=0&_nc_zt=23&_nc_ht=instagram.fepa8-1.fna&_nc_gid=6dU_cuKV1r1c8LI2FGFfCQ&_nc_ss=7a22e&oh=00_AQBaW2FzunI3JebOb0I1iyljnuHDrPTExlkbyTsB6bNrFQ&oe=6A5E2491'
     ]
   },
   {
     id: 'port-6',
     title: 'Lágrimas del Zodiaco',
-    imageUrl: '/assets/images/portfolio_eyes_1782922794305.jpg',
+    imageUrl: 'https://instagram.fepa8-1.fna.fbcdn.net/v/t51.82787-15/649171431_18089665520515789_2507383293027811095_n.webp?stp=dst-webp_p720x720&_nc_cat=107&ig_cache_key=Mzg0OTI0NTAzMzgyNDcyMTE4Mw%3D%3D.3-ccb7-5&ccb=7-5&_nc_sid=58cdad&efg=eyJ2ZW5jb2RlX3RhZyI6IkNBUk9VU0VMX0lURU0ueHBpZHMuMTA4MC5zZHIucmVndWxhcl9waG90by5DMyJ9&_nc_ohc=1aUG5Efch1gQ7kNvwHyDGLP&_nc_oc=Adpt3dNU8BwNSK3G19je14q-qm_7jmHNUV4ZY4ix-h8ZzlytpKmKoo1sggnjUXS2bF8aHT9E8xawVg3Aeg3dOEhL&_nc_ad=z-m&_nc_cid=0&_nc_zt=23&_nc_ht=instagram.fepa8-1.fna&_nc_gid=6dU_cuKV1r1c8LI2FGFfCQ&_nc_ss=7a22e&oh=00_AQCcI-JPLFIBwzA1s6eTeZvI9_46jmgoSAKsE0GWjDN3Dw&oe=6A5E047B',
     artistId: 'art-2',
     bodyPart: 'Cuello',
     style: 'Fineline',
@@ -173,7 +173,7 @@ export const PORTFOLIO_ITEMS: PortfolioItem[] = [
     colorType: 'Black & Grey',
     description: 'Constelaciones astronómicas y órbitas celestes dibujadas con elegancia suprema bajo la base del lóbulo.',
     healingPhotos: [
-      '/assets/images/portfolio_eyes_1782922794305.jpg'
+      'https://instagram.fepa8-1.fna.fbcdn.net/v/t51.82787-15/649171431_18089665520515789_2507383293027811095_n.webp?stp=dst-webp_p720x720&_nc_cat=107&ig_cache_key=Mzg0OTI0NTAzMzgyNDcyMTE4Mw%3D%3D.3-ccb7-5&ccb=7-5&_nc_sid=58cdad&efg=eyJ2ZW5jb2RlX3RhZyI6IkNBUk9VU0VMX0lURU0ueHBpZHMuMTA4MC5zZHIucmVndWxhcl9waG90by5DMyJ9&_nc_ohc=1aUG5Efch1gQ7kNvwHyDGLP&_nc_oc=Adpt3dNU8BwNSK3G19je14q-qm_7jmHNUV4ZY4ix-h8ZzlytpKmKoo1sggnjUXS2bF8aHT9E8xawVg3Aeg3dOEhL&_nc_ad=z-m&_nc_cid=0&_nc_zt=23&_nc_ht=instagram.fepa8-1.fna&_nc_gid=6dU_cuKV1r1c8LI2FGFfCQ&_nc_ss=7a22e&oh=00_AQCcI-JPLFIBwzA1s6eTeZvI9_46jmgoSAKsE0GWjDN3Dw&oe=6A5E047B'
     ]
   }
 ];
@@ -188,7 +188,7 @@ export const FLASH_ITEMS: FlashItem[] = [
     availability: 'disponible',
     isLimited: true,
     svgPath: '',
-    imageUrl: '/assets/images/flash_rabbits_circle_1782924506874.jpg'
+    imageUrl: 'https://i.pinimg.com/736x/3b/6f/d8/3b6fd8ea556e99c8d096f1829c0cef94.jpg'
   },
   {
     id: 'fls-2',
@@ -199,7 +199,7 @@ export const FLASH_ITEMS: FlashItem[] = [
     availability: 'reservado',
     isLimited: false,
     svgPath: '',
-    imageUrl: '/assets/images/flash_bunny_heart_1782924523290.jpg'
+    imageUrl: 'https://i.pinimg.com/736x/66/ab/2b/66ab2bad41eba7b6d71ed2bef44fe7eb.jpg'
   },
   {
     id: 'fls-3',
@@ -210,7 +210,7 @@ export const FLASH_ITEMS: FlashItem[] = [
     availability: 'vendido',
     isLimited: true,
     svgPath: '',
-    imageUrl: '/assets/images/flash_gothic_sigil_1782924536837.jpg'
+    imageUrl: 'https://i.pinimg.com/736x/01/07/f8/0107f8fcfaf985242f8293b167f19640.jpg'
   },
   {
     id: 'fls-4',
@@ -221,6 +221,6 @@ export const FLASH_ITEMS: FlashItem[] = [
     availability: 'disponible',
     isLimited: false,
     svgPath: '',
-    imageUrl: '/assets/images/flash_argentina_stamp_1782924552442.jpg'
+    imageUrl: 'https://i.pinimg.com/1200x/67/12/b1/6712b15fdf27542db769e1a1f9d41561.jpg'
   }
 ];
